532ead399e74131a0b9441cdbdf7f977f6f77a72
Sofie Friis Jespersen <122779383+SofieFriisJ@users.noreply.github.com>
2024-08-11 21:14:41 +0200

    Add files via upload

---
+++ scripts/index.js
@@ -0,0 +1,19 @@
+/* 
+
+----- Udskriv acoordions via JS: -----
+- lav et array med objekter (et objekt for hver accordion)
+    - Hvert object skal have følgende keys: title og bodytext 
+
+- for at få dem udskrevet, skal der forEach'es over array'et med objekter
+- i forEach'en udskrives html'en hver accordion (inde i ``)
+
+
+
+
+----- Funktionalitet (åbn & lukke): -----
+- Hver accordion har class'en .accordion. gem dem ned i en variabel ( brug querySelectorAll), og foreach over dem, så hver enkel kan trykkes på
+    - brug e.currentTarget
+- husk 'classList.toggle' istedet for 'add'
+
+*/
+
